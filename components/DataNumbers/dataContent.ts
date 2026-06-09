4a63ce16fca4a7afb22bd62ae011f33d9e166556
Myzolz <lucasrocha7455@gmail.com>
2026-06-09 17:47:47 -0300

    cards-section

---
+++ components/DataNumbers/dataContent.ts
@@ -0,0 +1,11 @@
+import { HeartHandshake, ThumbsUp, Cake, HousePlus } from "lucide-react";
+import { LucideIcon } from "lucide-react";
+
+export type DataContent = { icon: LucideIcon; titulo: string; numero: string };
+
+export const dataContent: DataContent[] = [
+    { icon: HousePlus, titulo: "Famílias Atendidas", numero: "+250" },
+    { icon: ThumbsUp, titulo: "Pessoas Impactadas", numero: "+400" },
+    { icon: Cake, titulo: "Anos de atuação", numero: "+25" },
+    { icon: HeartHandshake, titulo: "Ações Efetuadas", numero: "+65" }
+];
