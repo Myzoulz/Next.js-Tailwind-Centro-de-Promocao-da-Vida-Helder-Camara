7eb0b10062d06f28af4b8c7c61ea9f2e3a31b7bd
Myzolz <lucasrocha7455@gmail.com>
2026-06-16 14:53:57 -0300

    about-page

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,19 @@
-import Header from "@/components/Header";
 import Hero from '../components/Hero/index';
 import DataNumbers from "@/components/DataNumbers";
 import CardsSection from "@/components/CardsSection";
 import AboutSection from '../components/AboutSection/index';
 import DonateSection from "@/components/DonateSection";
 import PartnersSection from "@/components/PartnersSection";
-import Footer from '../components/Footer/index';
 
 export default function Home() {
   return (
     <main>
-      <section className="relative md:h-dvh w-full shadow-lg overflow-hidden">
-        <Header />
-        <Hero />
-      </section>
+      <Hero />
       <CardsSection />
       <DataNumbers />
       <AboutSection />
       <DonateSection />
       <PartnersSection />
-      <Footer />
     </main>
   );
 }
--- components/DonateSection/index.tsx
+++ components/DonateSection/index.tsx
@@ -1,10 +1,14 @@
+"use client";
 import { Heart } from "lucide-react";
+import { useInView } from "react-intersection-observer";
 
 export default function DonateSection() {
+    const { ref, inView } = useInView({ triggerOnce: true });
+
     return (
         <section
             className="relative bg-parallax h-70 overflow-hidden"
-            style={{ 
+            style={{
                 backgroundImage: "url('/donate-background.png')",
                 backgroundAttachment: "fixed"
             }}
@@ -13,7 +17,13 @@ export default function DonateSection() {
                 <h2 className="main-texts leading-normal">Contribua com a organização</h2>
                 <h1 className="text-3xl font-sans font-semibold text-zinc-100">Doar é um ato de amor</h1>
                 <Heart />
-                <a href="/donate" className="default-btn w-80 h-15 text-2xl bg-amber-200 hover:bg-amber-300 text-brand-800">
+                <a
+                    ref={ref}
+                    href="/donate"
+                    className={`default-btn w-80 h-15 text-2xl bg-amber-200 hover:bg-amber-300 text-brand-800 ${
+                        inView ? "animate-zoomIn" : "invisible"
+                    }`}
+                >
                     Como Ajudar
                 </a>
             </div>
