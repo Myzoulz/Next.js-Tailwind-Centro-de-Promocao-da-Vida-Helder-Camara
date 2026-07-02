0817540a04fc06a8aeabe2294b5574ec89e98df2
Myzolz <lucasrocha7455@gmail.com>
2026-07-02 14:41:52 -0300

    initial-news-page

---
+++ app/news/page.tsx
@@ -0,0 +1,10 @@
+export default function NewsPage() {
+    return (
+        <main className="flex-1 flex flex-col items-center justify-center text-zinc-900">
+            <h1 className="text-4xl font-bold mb-4">Notícias</h1>
+            <p className="text-lg text-zinc-700">
+                Bem-vindo à página de notícias do CPVHC. Aqui você encontrará as últimas atualizações e informações sobre nossas atividades e projetos.
+            </p>
+        </main>
+    );
+}
