17b4e1b1f8b228d255bde274783913a9d368d68f
Myzolz <lucasrocha7455@gmail.com>
2026-09-21 22:09:21 -0300

    voluntariado: colagem usa fotos da pasta imagesEquipe (remove equipe.jpeg da raiz)

--- components/VoluntariadoSection/Collage.tsx
+++ components/VoluntariadoSection/Collage.tsx
@@ -8,20 +8,20 @@ export default function Collage() {
         <section ref={ref} className="relative w-full max-w-2xl aspect-[4/3]">
 
             <img
-                src="/equipe.jpeg"
+                src="/imagesEquipe/equipe.jpeg"
                 alt="Equipe do CPVHC"
                 className="absolute left-0 top-0 w-[65%] h-full object-cover rounded-xl"
             />
 
             <img
-                src="/equipe.jpeg"
+                src="/imagesEquipe/equipe-2.jpeg"
                 alt="Equipe do CPVHC"
                 className={`absolute right-0 top-1/5 w-[45%] aspect-video object-cover rounded-xl shadow-lg
                     ${inView ? 'animate-fadeSlide [animation-duration:1300ms]' : 'opacity-0'}`}
             />
 
             <img
-                src="/equipe.jpeg"
+                src="/imagesEquipe/equipe-3.jpeg"
                 alt="Equipe do CPVHC"
                 className={`absolute right-16 bottom-0 w-[55%] aspect-video object-cover object-bottom rounded-xl shadow-lg
                     ${inView ? 'animate-fadeSlide [animation-duration:2100ms]' : 'opacity-0'}`}
