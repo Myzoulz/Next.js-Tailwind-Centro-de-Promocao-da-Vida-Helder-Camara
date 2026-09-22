354d47329697ca9135cb31e9c224f19aad0a011d
Myzolz <lucasrocha7455@gmail.com>
2026-09-21 22:13:17 -0300

    voluntariado: troca posicao das fotos na colagem e atualiza foto da equipe

--- components/VoluntariadoSection/Collage.tsx
+++ components/VoluntariadoSection/Collage.tsx
@@ -14,14 +14,14 @@ export default function Collage() {
             />
 
             <img
-                src="/imagesEquipe/equipe-2.jpeg"
+                src="/imagesEquipe/equipe-3.jpeg"
                 alt="Equipe do CPVHC"
                 className={`absolute right-0 top-1/5 w-[45%] aspect-video object-cover rounded-xl shadow-lg
                     ${inView ? 'animate-fadeSlide [animation-duration:1300ms]' : 'opacity-0'}`}
             />
 
             <img
-                src="/imagesEquipe/equipe-3.jpeg"
+                src="/imagesEquipe/equipe-2.jpeg"
                 alt="Equipe do CPVHC"
                 className={`absolute right-16 bottom-0 w-[55%] aspect-video object-cover object-bottom rounded-xl shadow-lg
                     ${inView ? 'animate-fadeSlide [animation-duration:2100ms]' : 'opacity-0'}`}
